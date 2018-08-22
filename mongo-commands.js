22672a5821e29e0b1b20791e8c34b015f77b8209
ovvn <omazinov@gmail.com>
2018-08-22 16:59:44 +0300

    Minor.

--- mongo-commands.js
+++ mongo-commands.js
@@ -1,3 +1,3 @@
 db.getCollectionNames().forEach(function(c) {if(c.indexOf('o_') < 0) return; db[c].remove(({'m.ts': {'$lt': new Date().getTime() - 7 * 24 * 60 * 60 * 1000}}));})
-db.tasks.remove(({'createdAt': {'\$lt': new Date().getTime() - 24 * 60 * 60 * 1000}}))
-db.o_tasks.remove(({'m.ts': {'\$lt': new Date().getTime() - 24 * 60 * 60 * 1000}}))
+db.tasks.remove(({'createdAt': {'$lt': new Date().getTime() - 24 * 60 * 60 * 1000}}))
+db.o_tasks.remove(({'m.ts': {'$lt': new Date().getTime() - 24 * 60 * 60 * 1000}}))
