0f7827e821a9fe819c18fbe75cd65f9e78bc0a5f
ovvn <omazinov@gmail.com>
2019-03-07 13:49:57 +0300

    Clean ops older than 5 days, tasks older than one hour without startTime.

--- mongo-commands.js
+++ mongo-commands.js
@@ -1,5 +1,5 @@
-db.getCollectionNames().forEach(function(c) {if(c.indexOf('o_') < 0) return; db[c].remove({'m.ts': {'$lt': new Date().getTime() - 7 * 24 * 60 * 60 * 1000}});})
+db.getCollectionNames().forEach(function(c) {if(c.indexOf('o_') < 0) return; db[c].remove({'m.ts': {'$lt': new Date().getTime() - 5 * 24 * 60 * 60 * 1000}});})
 db.getCollectionNames().forEach(function(c) {if(c.indexOf('system') >= 0 || c.indexOf('o_') >= 0 || c.indexOf('sessions') >= 0) return; db[c].remove({'_type': {'$type': 10}});})
-db.tasks.remove({'createdAt': {'$lt': new Date().getTime() - 60 * 60 * 1000}, 'status': {'$ne': 'new'}})
+db.tasks.remove({'createdAt': {'$lt': new Date().getTime() - 60 * 60 * 1000}, 'startTime': {'$exists': false}})
 db.o_tasks.remove({'m.ts': {'$lt': new Date().getTime() - 60 * 60 * 1000}})
 quit()
